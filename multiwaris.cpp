32cf470684ef39c5baccb8c0198fda8484fb60c4
aryarezalfarezi-cmyk <aryarezalfarezi@gmail.com>
2026-05-19 11:18:22 +0700

    membuat class pekerja

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -11,3 +11,12 @@ public:
              << endl;
     }
 };
+class pekerja : virtual public orang
+{
+public:
+    pekerja(int pUmur) : orang(pUmur)
+    {
+        cout << "pekerja dibuat\n"
+             << endl;
+    }
+};
